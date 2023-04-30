a3fa8cc24c57f5bdc090145ecc3e5c8d7677e299
Alfa <210711378@students.uajy.ac.id>
2023-04-30 15:07:31 +0700

    inisiasi semua class

---
+++ src/gd8_a_11378/GD8_A_11378.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gd8_a_11378;
+
+/**
+ *
+ * @author ASUS
+ */
+public class GD8_A_11378 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
